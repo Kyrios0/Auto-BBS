e07ab20413475d8edbc8241c41b3e400957c18de
Kyrios0 <1120626222@qq.com>
2019-03-08 19:17:02 +0800

    Update index
    add forum box

--- bbs/app_bbs/static/js/index.jsx
+++ bbs/app_bbs/static/js/index.jsx
@@ -14,51 +14,55 @@ var TopBar = createReactClass({
 
     render: function() {
         return (
-            <div>
-                <ul>
-                <li><a href="#home"><img src="https://blog.kyrios.cn/wp-content/uploads/2019/03/logo_50px.png"/></a></li>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a className="active" href="#login">Login</a></li>
-                </ul>
+            <div id="mainmenu">
+                <div className="right">
+                    <div className="li"><a className="active" href="#login">Login</a></div>
+                </div>
+                <div>
+                    <div className="ul">
+                        <div className="li"><a href="#home"><img src="https://blog.kyrios.cn/wp-content/uploads/2019/03/logo_50px.png"/></a></div>
+                        <div className="li"><a href="#home">Home</a></div>
+                        <div className="li"><a href="#about">About</a></div>
+                        <div className="li"><a href="#contact">Contact</a></div>
+                    </div>
+                </div>
+                
                 <style jsx>{`
-                    ul {
+                    #mainmenu {
+                        overflow: hidden;
+                        margin: 0 0 198px 0;
+                    }
+                    .ul {
                         list-style-type: none;
                         margin: 0;
                         padding: 0;
                         overflow: hidden;
                         background-color: #fff0cd;
                     }
-                      
-                    li {
+                    .li {
                         float: left;
                         border-right:1px solid #bbb;
                     }
-                      
-                    li:last-child {
+                    .right {
                         float: right;
                         border-right: none;
                     }
-                      
-                    li a {
+                    .li a {
                         display: block;
                         color: black;
                         text-align: center;
                         padding: 5px 16px;
                         text-decoration: none;
                         line-height: 55px;
-                    }
-                      
-                    li a:hover:not(.active) {
+                    } 
+                    .li a:hover:not(.active) {
                         background-color: #fff8e7;
                     }
-                      
-                    li a.active {
+                    .li a.active {
                         background-color: #591804;
                         color: #fff;
                     }
-                    li a.active:hover {
+                    .li a.active:hover {
                         background-color: #b87563;
                         color: #fff;
                     }
@@ -66,9 +70,9 @@ var TopBar = createReactClass({
             </div>
         )
     }
-})
+});
 
-var TopicBG = createReactClass({
+var TopBG = createReactClass({
     getInitialState: function() {
         return {
             
@@ -88,7 +92,7 @@ var TopicBG = createReactClass({
                 <style jsx>{`
                     .head_bg {
                         height: 190px;
-                        margin: 0px 0px -190px;
+                        margin: 65px 0px -255px 0px;
                         overflow: hidden;
                         background: rgba(0, 0, 0, 0) url("https://blog.kyrios.cn/wp-content/uploads/2019/03/bbs_head.jpg") repeat scroll left bottom;
                     }
@@ -112,9 +116,9 @@ var TopicBG = createReactClass({
             </div>
         )
     }
-})
+});
 
-var Forums = createReactClass({
+var Thread = createReactClass({
     getInitialState: function() {
         return {
 
@@ -123,12 +127,150 @@ var Forums = createReactClass({
 
     render: function() {
         return (
-            <div>
+            <React.Fragment>
+                <tbody>
+                    <tr className={"row"+(this.props.id%2+1)}>
+                        <td className="c1">
+                            <a className="replies"> { this.props.topic['replies'] } </a>
+                        </td>
+                        <td className="c2">
+                            <a className="topic"> { this.props.topic['topic_name'] } </a>
+                        </td>
+                        <td className="c3">
+                            <a className="author"> { this.props.topic['username'] } </a>
+                            <span className="postdate"> { this.props.topic['post_time'] } </span>
+                        </td>
+                        <td className="c4">
+                            <a className="replydate"> { this.props.topic['reply_time'] } </a>
+                        </td>
+                    </tr>
+                </tbody>
+                <style jsx>{`
+                    .replies {
+                        font-size: 1.667em;
+                        color: rgb(238, 209, 175);
+                    }
+                    .topic {
+                        font-size: 1.085em;
+                    }
+                    .postdate {
+                        display: block;
+                        overflow: hidden;
+                    }
+                    td {
+                        padding: 6px;
+                        line-height: 1.83em;
+                        word-wrap: break-word;
+                        word-break: break-all;
+                        text-align: left;
+                    }
+                    .c1 {
+                        width: 5%;
+                        text-align: center;
+                        padding-left: 1px;
+                        padding-right: 1px;
+                        white-space: nowrap;
+                    }
+                    .c2 {
+                        width: auto;
+                        word-wrap: break-word;
+                        word-break: break-all;
+                    }
+                    .c3, .c4 {
+                        width: 14%;
+                        text-align: center;
+                        color: #58697b;
+                        font-size: 0.923em;
+                    }
+                    .row1 .c1 {
+                        background-color: #fff6df;
+                    }
+                    .row1 .c2 {
+                        background-color: #fff8e7;
+                    }
+                    .row1 .c3 {
+                        background-color: #fff6df;
+                    }
+                    .row1 .c4 {
+                        background-color: #fff8e7;
+                    }
+                    .row2 .c1 {
+                        background-color: #ffedc3;
+                    }
+                    .row2 .c2 {
+                        background-color: #fff0cd;
+                    }
+                    .row2 .c3 {
+                        background-color: #ffedc3;
+                    }
+                    .row2 .c4 {
+                        background-color: #fff0cd;
+                    }
+                `}</style>
+            </React.Fragment>
+        )
+    }
+});
 
+var Forums = createReactClass({
+    getInitialState: function() {
+        return {
+            topics: [
+                {
+                    "tid" : 1, 
+                    "topic_name": "皇后局都是这样打游戏的？那这个游戏怕是不适合我", 
+                    "topic_type" : "", 
+                    "uid" : 233, 
+                    "username" : "blacsheep", 
+                    "post_time" : "Thu Mar 7 18:30:58 2019",
+                    "reply_time": "Thu Mar 7 18:31:58 2019", 
+                    "replies": 1
+                }, {
+                    "tid" : 2, 
+                    "topic_name": "被6战支配的我终于舒服了，4亡灵巨魔骑艰难吃下精英战", 
+                    "topic_type" : "讨论一波", 
+                    "uid" : 234, 
+                    "username" : "TGWarWolf", 
+                    "post_time" : "Thu Mar 7 18:35:58 2019",
+                    "reply_time": "Thu Mar 7 18:37:58 2019",
+                    "replies": 10
+                },
+            ], 
+        };
+    },
+
+    render: function() {
+        var topicList = [];
+        for (var i = 0; i < this.state.topics.length; i++){
+            topicList.push(
+                <Thread topic={this.state.topics[i]} id={i}/>
+            );
+        }
+        return (
+            <div className="mod_wrap">
+                <table className="forumbox">
+                    { topicList }
+                </table>
+                <style jsx>{`
+                    .mod_wrap {
+                        text-align: center;
+                        margin: 0px 10px 8px 10px;
+                    }
+                    .forumbox {
+                        width: 100%;
+                        border-spacing: 1px;
+                        border: 1px solid #cacaca;
+                        box-shadow: 0 0 5px -3px #000;
+                        border-radius: 2.5px;
+                        margin: 0 0 8px 0;
+                        background-color: #fffcee;
+                    }
+                    
+                `}</style>
             </div>
         )
     }
-})
+});
 
 var BBS = createReactClass({
     getInitialState: function() {
@@ -140,13 +282,13 @@ var BBS = createReactClass({
     render: function() {
         return (
             <div>
+                <TopBG/>
                 <TopBar/>
-                <TopicBG/>
                 <Forums/>
             </div>
         )
     }
-})
+});
 
 ReactDOM.render(
     <div>
