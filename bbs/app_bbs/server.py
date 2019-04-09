cd111e373872f52d692e357868410261d5500ab2
blacsheep <784522717@qq.com>
2019-04-10 01:02:02 +0800

    Add some route and set config

--- bbs/app_bbs/server.py
+++ bbs/app_bbs/server.py
@@ -1,24 +1,18 @@
-from flask import Flask, request, render_template
-from flask_restful import Resource, Api
+from flask import Flask
+from app_bbs.resources.topic import *
+from app_bbs.resources.login import *
+from app_bbs.resources.user import *
+from app_bbs.resources.posts import *
+import config
 
 app = Flask(__name__)
 api = Api(app)
+app.config.from_object(config)
 
-@app.route('/')
-def index():
-    return render_template('dev.html')
-    
-class BBSAPI(Resource):
-    """
-    API for BBS
-    """
-    def get(self):
-        pass
-    
-    def post(self):
-        pass
-
-api.add_resource(BBSAPI, '/api')
+api.add_resource(Topic_api, '/api/topic', '/api/topic/<int:tid>', '/api/topic/page/<int:page_num>')
+api.add_resource(Login_api, '/api/login')
+api.add_resource(User_api, '/api/users', '/api/users/<int:uid>')
+api.add_resource(Posts_api, '/api/posts/<int:tid>', '/api/posts/<int:tid>/<int:rid>', '')
 
 if __name__ == '__main__':
     app.run()
