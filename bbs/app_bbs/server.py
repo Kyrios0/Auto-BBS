ccfb7a8868f174683ac6dddb635b58ec73dc88c8
blacsheep <784522717@qq.com>
2019-05-04 23:56:07 +0800

    Add router of agree and comment

--- bbs/app_bbs/server.py
+++ bbs/app_bbs/server.py
@@ -3,6 +3,7 @@ from app_bbs.resources.topic import *
 from app_bbs.resources.login import *
 from app_bbs.resources.user import *
 from app_bbs.resources.posts import *
+from app_bbs.resources.agree import *
 import config
 
 app = Flask(__name__)
@@ -12,7 +13,14 @@ app.config.from_object(config)
 api.add_resource(Topic_api, '/api/topic', '/api/topic/<int:tid>', '/api/topic/page/<int:page_num>')
 api.add_resource(Login_api, '/api/login')
 api.add_resource(User_api, '/api/users', '/api/users/<int:uid>')
-api.add_resource(Posts_api, '/api/posts/<int:tid>', '/api/posts/<int:tid>/<int:rid>')
+api.add_resource(Posts_api, '/api/posts/<int:tid>',
+                 '/api/posts/<int:tid>/<int:rid>',
+                 '/api/posts/<int:tid>/<int:rid>/<int:rrid>')
+
+api.add_resource(Agree_api, '/api/agree/<int:tid>',
+                 '/api/agree/<int:tid>/<int:rid>',
+                 '/api/agree/<int:tid>/<int:rid>/<int:rrid>')
+
 
 if __name__ == '__main__':
     app.run()
